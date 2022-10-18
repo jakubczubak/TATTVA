1f662c4f165421da8f3da5ea7927ced544950a09
Kuba <czubakjakub94@gmail.com>
2022-10-18 20:27:41 +0200

    added Date.js

--- src/modal/modal-manager.js
+++ src/modal/modal-manager.js
@@ -1,12 +1,11 @@
-import { Modal } from "../common/modal";
+import { Modal } from "../views/Modal";
 import { Cart } from "../views/Cart";
 
 export const modalManger = {
-
   createModal(value, acceptBtnFunction, declineBtnFunction) {
     const modalContainer = document.createElement("div");
     modalContainer.className = "modal-container";
-  
+
     modalContainer.innerHTML = Modal({
       type: value.type,
       title: value.title,
@@ -14,27 +13,27 @@ export const modalManger = {
       accept_btn: value.accept_btn,
       decline_btn: value.decline_btn,
     });
-  
+
     document.body.appendChild(modalContainer);
-  
+
     const closeModalIcon = document.querySelector(".modal-close-icon");
-  
+
     closeModalIcon.addEventListener("click", () => {
       const modal = document.querySelector(".modal-container");
       modal.remove();
       document.body.style.overflow = "auto";
     });
-  
+
     const acceptBtn = document.querySelector(".modal-accept");
-  
+
     acceptBtn.addEventListener("click", acceptBtnFunction);
-  
+
     const declineBtn = document.querySelector(".modal-decline");
-  
+
     declineBtn.addEventListener("click", declineBtnFunction);
-  
+
     const modalActive = document.querySelector(".modal");
-  
+
     modalActive.focus();
     modalActive.addEventListener("keydown", (e) => {
       if (e.key === "Escape") {
@@ -43,16 +42,16 @@ export const modalManger = {
         document.body.style.overflow = "auto";
       }
     });
-  
+
     const modalActiveContainer = document.querySelector(".modal-container");
-  
+
     modalActiveContainer.addEventListener("click", (e) => {
       if (e.target.classList.contains("modal-container")) {
         e.target.remove();
         document.body.style.overflow = "auto";
       }
     });
-  
+
     document.body.style.overflow = "hidden";
   },
 
@@ -61,15 +60,16 @@ export const modalManger = {
     modal.remove();
     document.body.style.overflow = "auto";
   },
-  
+
   goToCart() {
     const modal = document.querySelector(".modal-container");
     modal.remove();
     document.body.style.overflow = "auto";
-    const main = document.querySelector("main");
-    main.innerHTML = "";
-    main.append(Cart());
-  }
-}
 
+    const navigateEvent = new CustomEvent("navigate", {
+      detail: () => Cart(),
+    });
 
+    document.body.dispatchEvent(navigateEvent);
+  },
+};
