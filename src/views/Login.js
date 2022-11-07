8eeeda08cd453c658856b270e0a29d236ad487d0
Kuba <czubakjakub94@gmail.com>
2022-11-07 15:21:31 +0100

    added login component

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,22 @@
 
 
 export function Login(){
+    
+    const loginContainer = document.createElement('div');
+    loginContainer.classList.add('login-container');
 
-
-    return `
-    Tu powstanie komponent logowania
+    loginContainer.innerHTML = `
+    <div class="login">
+        <img src=${require("../assets/tattva-spa-vector-logo.svg")} alt="logo">
+        <h4>Sign in and start your SPA adventure</h4>
+        
+        <input  type="Email" placeholder="Email">
+        <input type="password" placeholder="Password">
+        <button>LOGIN</button>
+        <h4 class="login-forgot-password">Forgot password?</h4>
+        <p>Don’t have an account yet? <strong>Sign Up</strong></p>
+      </div>
     `
+
+    return loginContainer;
 }
